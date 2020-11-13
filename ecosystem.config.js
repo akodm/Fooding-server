404bb60dd27bfad5e519f108bd72047a35e8919f
조준명 <a8456452@naver.com>
2020-11-13 18:26:41 +0900

    pm2 기본 설정 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
   apps : [{
-    name: 'server',
+    name: 'SERVER',
     script: './bin/www',
-    instances: 1,
+    instances: 4,
     watch: true,
-    time : true,
     ignore_watch : ['node_modules','public'],
     exec_mode : 'cluster',
+    wait_ready: true,
+    max_restarts: 2,
+    listen_timeout: 10000,
     env: {
       NODE_ENV: 'development'
     },
     env_production: {
       NODE_ENV: 'production'
-    }
+    },
   }]
 };
